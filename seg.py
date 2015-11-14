8a92afc70fb15cde58ef663d73662b16408ada67
licstar <licstar@sina.com>
2015-11-14 20:59:18 +0800

    batch、初始化

--- seg.py
+++ seg.py
@@ -1,10 +1,16 @@
 # -*- coding: UTF-8 –*-
 import tensorflow as tf
 import time
+import math
+import numpy as np
 from datetime import datetime
 
 PAD_ID = 0
 UNK_ID = 1
+
+flags = tf.app.flags
+FLAGS = flags.FLAGS
+flags.DEFINE_float('batch_size', 4, 'batch size')
 
 #TODO 读取embedding，只记录词表中的词
 def read_embedding(file_name, vocab):
@@ -25,91 +31,103 @@ def create_vocab(data_path):
             vocab[word] = len(vocab)
   return vocab
 
+class DataSet(object):
 
-#输入
-#file_name文件名，window_size窗口大小，vocab词表
-#输入文件格式：在/0 这/1 个/2 激/1 动/3 人/3 心/2 的/0 时/1 刻/2 ，/0 我/0 很/0 高/1
-#返回
-#data  [0,0,在,这,个],[0,在,这,个,激],[在,这,个,激,动],[这,个,激,动,人],...
-#data会对应到词表编号
-#label [0,1,2,1,3,3,2,0,1,2...]
-def read_file(data_path, window_size, vocab):
-  data = []
-  label = []
-  hw = (window_size-1)/2 #half window
-  with open(data_path) as data_f:
-    for line in data_f:
-      data_line = [0, 0]
-      for node in line.strip().split(" "):
-        x = node.split("/")
-        data_line.append(vocab.get(x[0], UNK_ID))
-        label.append(int(x[1]) % 4) #数字、字母、混合可以/4得到
-      data_line.extend([0, 0])
-      for i in range(hw, len(data_line)-hw):
-        data.append([data_line[i+j] for j in range(-hw, hw+1)])
-  return data, label
+  def __init__(self, data_path, window_size, vocab, class_size):
+    self._data, self._label = self.read_file(data_path, window_size, vocab)
+    self._label = self.dense_to_one_hot(self._label, class_size)
+    self._data = np.array(self._data)
+    self._label = np.array(self._label)
+    self._index_in_epoch = 0
+    self._num_examples = len(self._data)
+    self._epochs_completed = 0
 
-'''
-def loss(logits, labels, batch_size, class_size):
-  sparse_labels = tf.reshape(labels, [batch_size, 1])
-  indices = tf.reshape(tf.range(0, batch_size, 1), [batch_size, 1])
-  concated = tf.concat(1, [indices, sparse_labels])
-  onehot_labels = tf.sparse_to_dense(concated, [batch_size, class_size], 1.0, 0.0)
+  #输入
+  #file_name文件名，window_size窗口大小，vocab词表
+  #输入文件格式：在/0 这/1 个/2 激/1 动/3 人/3 心/2 的/0 时/1 刻/2 ，/0 我/0 很/0 高/1
+  #返回
+  #data  [0,0,在,这,个],[0,在,这,个,激],[在,这,个,激,动],[这,个,激,动,人],...
+  #data会对应到词表编号
+  #label [0,1,2,1,3,3,2,0,1,2...]
+  def read_file(self, data_path, window_size, vocab):
+    data = []
+    label = []
+    hw = (window_size-1)/2 #half window
+    with open(data_path) as data_f:
+      for line in data_f:
+        data_line = [0, 0]
+        for node in line.strip().split(" "):
+          x = node.split("/")
+          data_line.append(vocab.get(x[0], UNK_ID))
+          label.append(int(x[1]) % 4) #数字、字母、混合可以/4得到
+        data_line.extend([0, 0])
+        for i in range(hw, len(data_line)-hw):
+          data.append([data_line[i+j] for j in range(-hw, hw+1)])
+    return data, label
 
-  cross_entropy = tf.nn.softmax_cross_entropy_with_logits(logits, onehot_labels)
-  return tf.reduce_mean(cross_entropy, name='cross_entropy')
-'''
-#def step():
+  def next_batch(self, batch_size):
+    """Return the next `batch_size` examples from this data set."""
 
-#TODO 先搞个能跑但是不对的版本
-batch_pos = 0
-def get_batch(data, label, batch_size):
-  batch_data = []
-  batch_label = []
-  global batch_pos
-  for i in range(0, batch_size):
-    batch_data.append(data[batch_pos])
-    batch_label.append(label[batch_pos])
-    batch_pos = batch_pos + 1
-    if batch_pos == len(data):
-      batch_pos = 0
-  return batch_data, batch_label
+    start = self._index_in_epoch
+    self._index_in_epoch += batch_size
+    if self._index_in_epoch > self._num_examples:
+      # Finished epoch
+      self._epochs_completed += 1
+      # Shuffle the data
+      perm = np.arange(self._num_examples)
+      np.random.shuffle(perm)
+      self._data = self._data[perm]
+      self._label = self._label[perm]
+      # Start next epoch
+      start = 0
+      self._index_in_epoch = batch_size
+      assert batch_size <= self._num_examples
+    end = self._index_in_epoch
+    return self._data[start:end], self._label[start:end]
 
-def dense_to_one_hot(labels_dense, class_size):
-  ret = []
-  for i in range(0, len(labels_dense)):
-    line = [0] * class_size
-    #print len(line), labels_dense[i]
-    line[labels_dense[i]] = 1
-    ret.append(line)
-  return ret
+  def dense_to_one_hot(self, labels_dense, class_size):
+    ret = []
+    for i in range(0, len(labels_dense)):
+      line = [0] * class_size
+      line[labels_dense[i]] = 1
+      ret.append(line)
+    return ret
 
-if __name__ == "__main__":
+  def get_data(self):
+    return self._data
+
+  def get_label(self):
+    return self._label
+
+  def get_epoch(self):
+    return self._epochs_completed
+
+def main(_):
   #read_embedding("embedding.txt")
 
   window_size = 5 #需要奇数
   vec_size = 50
-  hidden_size = 50
+  hidden_size = 100
   class_size = 4
-  batch_size = 32
+  batch_size = int(FLAGS.batch_size)
   input_size = vec_size * window_size
 
   #读取文件
   vocab = create_vocab("seg_train.txt")
-  data_train, label_train = read_file("seg_train.txt", window_size, vocab)
-  data_valid, label_valid = read_file("seg_valid.txt", window_size, vocab)
-  data_test, label_test = read_file("seg_test.txt", window_size, vocab)
-  
-  label_train = dense_to_one_hot(label_train, class_size) #TODO 先用这个顶着，看看怎么优化
-  label_test = dense_to_one_hot(label_test, class_size)
-  label_valid = dense_to_one_hot(label_valid, class_size)
 
-  print len(label_train)
+  train = DataSet("seg_train.txt", window_size, vocab, class_size)
+  valid = DataSet("seg_valid.txt", window_size, vocab, class_size)
+  test = DataSet("seg_test.txt", window_size, vocab, class_size)
+
+  print len(train.get_data())
 
   #所有变量
-  embeddings = tf.Variable(tf.zeros([len(vocab), vec_size]))
-  W1 = tf.Variable(tf.zeros([input_size, hidden_size]))
-  W2 = tf.Variable(tf.zeros([hidden_size, class_size]))
+  embeddings = tf.Variable(tf.random_uniform([len(vocab), vec_size], 
+    -0.5, 0.5))
+  W1 = tf.Variable(tf.random_uniform([input_size, hidden_size], 
+    -0.5/math.sqrt(input_size), 0.5/math.sqrt(input_size)))
+  W2 = tf.Variable(tf.random_uniform([hidden_size, class_size], 
+    -0.5/math.sqrt(hidden_size), 0.5/math.sqrt(hidden_size)))
   b1 = tf.Variable(tf.zeros([hidden_size]))
   b2 = tf.Variable(tf.zeros([class_size]))
   
@@ -117,63 +135,67 @@ if __name__ == "__main__":
   ids = tf.placeholder("int32", shape=[None, window_size])
   y_ = tf.placeholder("float", shape=[None, class_size])
 
-  #生成batch
-  #data_train = tf.convert_to_tensor(data_train)
-  #data_train = tf.train.slice_input_producer(data_train)
-  #label_train = tf.train.slice_input_producer(label_train)
-  #data_batch, label_batch = tf.train.shuffle_batch(
-  #   [data_train, label_train], batch_size,
-  #    capacity=50000, min_after_dequeue=10000)
-
   #网络结构
   ids_embedding = tf.nn.embedding_lookup(embeddings, ids)
   x = tf.reshape(ids_embedding, [-1, vec_size * window_size])
   h = tf.tanh(tf.matmul(x, W1) + b1)
   y = tf.nn.softmax(tf.matmul(h, W2) + b2)
   cross_entropy = -tf.reduce_sum(y_*tf.log(y))
+
+  #调试信息
+  tf.histogram_summary('x', x)
+  tf.histogram_summary('h', h)
+  tf.histogram_summary('y', y)
+  tf.histogram_summary('W1', W1)
+  tf.histogram_summary('W2', W2)
+  tf.histogram_summary('b1', b1)
+  tf.histogram_summary('b2', b2)
   tf.scalar_summary('cross_entropy', cross_entropy)
-  #tf.histogram_summary('cross_entropy', cross_entropy)
-  #cross_entropy = loss(logits, y_, batch_size, class_size)
-  summary_op = tf.merge_all_summaries()
 
-  train_step = tf.train.GradientDescentOptimizer(0.01).minimize(cross_entropy)
 
-  
   with tf.Session() as sess:
     sess.run(tf.initialize_all_variables())
-    #tf.train.start_queue_runners(sess=sess)
+    
+    train_step = tf.train.GradientDescentOptimizer(0.01).minimize(cross_entropy)
 
+    summary_op = tf.merge_all_summaries()
     summary_writer = tf.train.SummaryWriter("/tmp/seg", graph_def=sess.graph_def)
     
+    duration_total = 0
+    batch_count = 0
 
     for step in xrange(10000000):
         start_time = time.time()
-        batch_data, batch_label = get_batch(data_train, label_train, batch_size)
-        #print tf.convert_to_tensor(batch_data).get_shape().as_list()
-        #print batch_label
+
+        batch_data, batch_label = train.next_batch(batch_size)
         data_now = {ids: batch_data, y_: batch_label}
         _, loss = sess.run([train_step, cross_entropy], feed_dict=data_now)
 
-        if step % 1000 == 0:
+        duration_total += time.time() - start_time
+        batch_count += 1
+
+        if step % 1000 == 0: #记录log
           summary_str = sess.run(summary_op, feed_dict=data_now)
           summary_writer.add_summary(summary_str, step)
-          
-        if step % 10000 == 0: #一次扫描结束
+
+        if step % 10000 == 0: #评测、屏幕输出
           correct_prediction = tf.equal(tf.argmax(y,1), tf.argmax(y_,1))
           accuracy = tf.reduce_mean(tf.cast(correct_prediction, "float"))
-          #loss = cross_entropy.eval(session=sess, feed_dict={ids: data_train, y_: label_train})
-          train_acc = sess.run(accuracy, feed_dict={ids: data_train, y_: label_train})
-          valid_acc = sess.run(accuracy, feed_dict={ids: data_valid, y_: label_valid})
-          test_acc = sess.run(accuracy, feed_dict={ids: data_test, y_: label_test})
-          #_, loss_value = sess.run([train_op, loss])
-
-          duration = time.time() - start_time
-          examples_per_sec = batch_size / float(duration)
-          sec_per_batch = float(duration)
-
-          format_str = ('%s: step %d, loss = %.2f %.2f %.2f %.2f (%.1f examples/sec; %.3f '
-                          'sec/batch)')
-          print (format_str % (datetime.now(), step, loss, train_acc, valid_acc, test_acc,
-                                 examples_per_sec, sec_per_batch))
-
           
+          train_acc = sess.run(accuracy, feed_dict={ids: train.get_data(), y_: train.get_label()})
+          valid_acc = sess.run(accuracy, feed_dict={ids: valid.get_data(), y_: valid.get_label()})
+          test_acc = sess.run(accuracy, feed_dict={ids: test.get_data(), y_: test.get_label()})
+          
+          examples_per_sec = batch_size * batch_count / float(duration_total)
+          sec_per_batch = float(duration_total) / batch_count
+          duration_total = 0
+          batch_count = 0
+
+          format_str = ('%s: step %d, epoch %d, loss = %.2f acc = %.2f %.2f %.2f (%.1f examples/sec; %.3f '
+                          'sec/batch)')
+          print (format_str % (datetime.now(), step, train.get_epoch(),
+                  loss, train_acc*100, valid_acc*100, test_acc*100,
+                  examples_per_sec, sec_per_batch))
+
+if __name__ == '__main__':
+  tf.app.run()
